524621d005154ba4d066ffb623f84b0dc9b1c2e1
ZzhangSamA <543606197@qq.com>
2020-09-15 17:55:55 +0800

    提交测试类

--- src/test/java/com/yanzhang/utils/IpUtilTest.java
+++ src/test/java/com/yanzhang/utils/IpUtilTest.java
@@ -9,23 +9,6 @@ public class IpUtilTest {
 
     @Test
     public void test1() {
-        String ipInfo = null;
-        try {
-            ipInfo = IpUtil.getLocalMac("127.0.0.1");
-        } catch (SocketException e) {
-            e.printStackTrace();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-        System.out.println(ipInfo);
-
-        try {
-            ipInfo = IpUtil.getLocalMac("192.168.5.16");
-        } catch (SocketException e) {
-            e.printStackTrace();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-        System.out.println(ipInfo);
+        System.out.println("hello world!!!");
     }
 }
